ee0e2ca1aa8d5540a76f6d047ccc8da464376160
Kevin Zhang <kevin.c.t.zhang@gmail.com>
2017-03-24 11:35:17 -0700

    refactored all print statements

--- BankersAlgorithm/main.c
+++ BankersAlgorithm/main.c
@@ -15,7 +15,7 @@ int currentAllocTable[5][5]={{2,0,1,1},
     {0,2,1,0},
     {1,0,3,0}};
 
-int max_claim[5][5]={{3,2,1,4},
+int maximumResources[5][5]={{3,2,1,4},
     {0,2,5,2},
     {5,1,0,5},
     {1,5,3,0},
@@ -38,21 +38,18 @@ void printCurrentlyAllocatedResouce();
 void printMaximumResource();
 void printAllocatedResources();
 void printResourcesinNeed();
+void calcAllocationTotal();
+void calcResourcesinNeed();
 
 int main(int argc, const char * argv[]) {
     printTotalSystemResource();
     printCurrentlyAllocatedResouce();
     printMaximumResource();
     
-    for (i = 0; i < processes; i++)
-        for (j = 0; j < resources; j++)
-            allocationTotal[j] += currentAllocTable[i][j];
-    
+    calcAllocationTotal();
     printAllocatedResources();
     
-    for (i = 0; i < resources; i++)
-        inNeed[i] = maxResource[i] - allocationTotal[i];
-    
+    calcResourcesinNeed();
     printResourcesinNeed();
     
     while (count != 0) {
@@ -61,7 +58,7 @@ int main(int argc, const char * argv[]) {
             if (running[i]) {
                 exec = 1;
                 for (j = 0; j < resources; j++) {
-                    if (max_claim[i][j] - currentAllocTable[i][j] > inNeed[j]) {
+                    if (maximumResources[i][j] - currentAllocTable[i][j] > inNeed[j]) {
                         exec = 0;
                         break;
                     }
@@ -113,7 +110,7 @@ void printMaximumResource(){
     printf("\nMaximum Resource:\n");
     for (i = 0; i < processes; i++) {
         for (j = 0; j < resources; j++)
-            printf("\t%d", max_claim[i][j]);
+            printf("\t%d", maximumResources[i][j]);
         printf("\n");
     }
 }
@@ -130,4 +127,15 @@ void printResourcesinNeed(){
     for (i = 0; i < resources; i++)
         printf("%d ", inNeed[i]);
     printf("\n");
+}
+
+void calcAllocationTotal(){
+    for (i = 0; i < processes; i++)
+        for (j = 0; j < resources; j++)
+            allocationTotal[j] += currentAllocTable[i][j];
+}
+
+void calcResourcesinNeed(){
+    for (i = 0; i < resources; i++)
+        inNeed[i] = maxResource[i] - allocationTotal[i];
 }
