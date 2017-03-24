5df6a2e889743c89bf9834f5e33636f40a2083f1
Kevin Zhang <kevin.c.t.zhang@gmail.com>
2017-03-23 17:29:23 -0700

    Initial Commit

---
+++ BankersAlgorithm/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  BankersAlgorithm
+//
+//  Created by Kai Zhang on 2017-03-23.
+//  Copyright © 2017 Kai Zhang. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
